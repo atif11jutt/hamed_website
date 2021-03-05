00eb195549d440b66170311d2f24a4f04f34a083
atif11jutt <atif11jutt@gmail.com>
2021-03-05 15:30:32 +0500

    friends page updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 jQuery(function($){
     console.log("jQuery loaded")
+
+    $(".translate").click(function(e){
+        e.preventDefault();
+        var arabic = $(this).parent().find(".card-text").data("arabic");
+        var eng = $(this).parent().find(".card-text").data("eng");
+        if($(this).hasClass("eng")){
+            $(this).parent().find(".card-text").html(arabic)
+            $(this).removeClass("eng")
+        }else{
+            $(this).parent().find(".card-text").html(eng)
+            $(this).addClass("eng")
+        }
+    })
 })
